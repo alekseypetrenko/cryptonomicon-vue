35689d8d034353105a81d5ac41769c4c75c92e0a
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-03-18 18:03:11 +0200

    Add websocket

--- src/App.vue
+++ src/App.vue
@@ -230,9 +230,9 @@ export default {
       this.tickers
         .filter((t) => t.name === tickerName)
         .forEach((t) => {
-          // if (t === this.selectedTicker) {
-          //   this.graph.push(price);
-          // }
+          if (t === this.selectedTicker) {
+            this.graph.push(price);
+          }
           t.price = price;
         });
     },
--- src/api.js
+++ src/api.js
@@ -2,41 +2,64 @@ const API_KEY =
   "8784928dc68813d2f33e331268f1c436c7d54f2ff040749388ffed7facdb1315";
 
 const tickersHandlers = new Map();
+const socket = new WebSocket(
+  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`,
+);
 
-const loadTickers = () => {
-  if (tickersHandlers.size === 0) {
+const AGGREGATE_INDEX = "5";
+socket.addEventListener("message", (e) => {
+  const {
+    TYPE: type,
+    FROMSYMBOL: currency,
+    PRICE: newPrice,
+  } = JSON.parse(e.data);
+
+  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
     return;
   }
 
-  fetch(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[
-      ...tickersHandlers.keys(),
-    ].join(",")}&tsyms=USD&api_key=${API_KEY}`,
-  )
-    .then((r) => {
-      return r.json();
-    })
-    .then((rawData) => {
-      const updatedPrices = Object.fromEntries(
-        Object.entries(rawData).map(([key, value]) => [key, value.USD]),
-      );
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach((fn) => fn(newPrice));
+});
 
-      Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-        const handlers = tickersHandlers.get(currency) ?? [];
-        handlers.forEach((fn) => fn(newPrice));
-      });
-    });
-};
+function sendToWebSocket(message) {
+  const stringifiedMessage = JSON.stringify(message);
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(stringifiedMessage);
+    return;
+  }
+  socket.addEventListener(
+    "open",
+    () => {
+      socket.send(stringifiedMessage);
+    },
+    { once: true },
+  );
+}
+
+function subscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~${ticker}~USD`],
+  });
+}
+
+function unsubscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubRemove",
+    subs: [`5~CCCAGG~${ticker}~USD`],
+  });
+}
 
 export const subscribeToTicker = (ticker, cb) => {
   const subscribers = tickersHandlers.get(ticker) || [];
   tickersHandlers.set(ticker, [...subscribers, cb]);
+  subscribeToTickerOnWs(ticker);
 };
 
 export const unsSbscribeFromTicker = (ticker) => {
   tickersHandlers.delete(ticker);
+  unsubscribeToTickerOnWs(ticker);
 };
-
-setInterval(loadTickers, 5000);
 
 window.tickersHandlers = tickersHandlers;
